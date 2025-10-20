bffea7b9df6b73110842744d6420a3e6063dd7f4
Aldik-droid <jarylkasin.a@gmail.com>
2025-10-20 16:18:51 +0500

    update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let isDay = true;
 modeBtn.addEventListener("click", () => {
   document.body.classList.toggle("night-mode");
   isDay = !isDay;
-  modeBtn.textContent = isDay ? "🌞 Switch to Night Mode" : "🌙 Switch to Day Mode";
+  modeBtn.textContent = isDay ? "Switch to Night Mode" : "Switch to Day Mode";
 });
 
 const pets = [
@@ -33,26 +33,26 @@ const petList = document.getElementById("petList");
 pets.forEach((pet, index) => {
   const div = document.createElement("div");
   div.className = "col-md-3";
-  div.innerHTML = 
-    <div class="card p-3 text-center">
-      <h4>${pet.name}</h4>
-      <p>${pet.type}</p>
-      <p>${pet.desc}</p>
-      <p class="hidden-text" id="extra${index}">More info about ${pet.name}!</p>
-      <button class="btn btn-outline-primary readBtn" data-id="${index}">Read More</button>
-      <div class="rating mt-2" data-id="${index}">
-        <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
-      </div>
-      <button class="btn btn-success mt-2 adoptBtn">Adopt Me</button>
+  div.innerHTML = `
+  <div class="card p-3 text-center">
+  <h4>${pet.name}</h4>
+  <p>${pet.type}</p>
+  <p>${pet.desc}</p>
+  <p class="hidden-text" id="extra${index}">More info about ${pet.name}!</p>
+  <button class="btn btn-outline-primary readBtn" data-id="${index}">Read More</button>
+  <div class="rating mt-2" data-id="${index}">
+    <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
     </div>
-  ;
+    <button class="btn btn-success mt-2 adoptBtn">Adopt Me</button>
+    </div>
+    `;
   petList.appendChild(div);
 });
 
 document.querySelectorAll(".readBtn").forEach(btn => {
   btn.addEventListener("click", e => {
     const id = e.target.getAttribute("data-id");
-    const extra = document.getElementById('extra${id}');
+    const extra = document.getElementById(`extra${id}`);
     extra.classList.toggle("hidden-text");
   });
 });
@@ -66,14 +66,14 @@ document.querySelectorAll(".rating").forEach((ratingDiv, index) => {
         stars[j].classList.add("active");
       }
       pets[index].rating = i + 1;
-      alert('${pets[index].name} rated ${pets[index].rating} stars!');
+      alert(`${pets[index].name} rated ${pets[index].rating} stars!`);
     });
   });
 });
 
 document.querySelectorAll(".adoptBtn").forEach(btn => {
   btn.addEventListener("click", () => {
-    const audio = new Audio('"https://cdn.pixabay.com/download/audio/2022/03/15/audio_78f79e6eb4.mp3?filename=ding-36029.mp3"');
+    const audio = new Audio("https://cdn.pixabay.com/download/audio/2022/03/15/audio_78f79e6eb4.mp3?filename=ding-36029.mp3");
     audio.play();
     alert("Thank you for adopting a pet!");
   });
